7f540398a0b13b2c54c6b2791e3f534bcccbe947
hicai <1021794763@qq.com>
2019-08-27 18:36:21 +0800

    登录

---
+++ vue-mtapp-master/components/index/artistic.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="m-istyle">
+    <dl @mouseover="enter">
+		  <dt>有格调</dt>
+		    <dd 
+			 v-for="(list,index) in nav" 			
+			 :key="index"
+			 :kind="list.kind"
+			 :class="{active:list.kind===kind}"	
+			 >
+        {{ list.name }}
+			</dd>
+			
+		</dl> 
+		<ul class="ibody">
+			<li v-for="(item, index) in cur" :key="index">
+          <el-card :body-style="{padding:'0px'}">
+           <img :src="item.url">
+			<div class="cbody">
+             <p class="title">{{ item.title }}</p>
+			 <p class="pos">{{ item.pos }}</p>
+			 <p class="price">￥<em>{{ item.price }}</em><span>/起</span></p>
+				 </div>
+			 </el-card>
+ 			</li>
+		</ul>
+	</div>	
+</template>
+ 
+<script>
+export default {
+	name:'',
+	data(){
+	 return {
+	 kind:'all',
+	 nav:[
+		{
+		  name:'全部',
+		  kind:'all'	
+		},{
+		  name:'约会聚餐',
+		  kind:'part'	
+		},{
+          name:'丽人SPA',
+		  kind:'sap'	
+		},
+		{
+          name:'电影演出',
+		  kind:'movie'	
+		}
+	 ],
+     list:{
+			all:[
+						{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'汉堡1',
+							pos:'地址1',
+							price:'25'
+						},{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'汉堡2',
+							pos:'地址2',
+							price:'25'
+						},{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'汉堡3',
+							pos:'地址3',
+							price:'25'
+						}
+					],
+					part:[
+						{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'标题1',
+							pos:'地址1',
+							price:'25'
+						},{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'标题2',
+							pos:'地址2',
+							price:'25'
+						},{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'标题3',
+							pos:'地址3',
+							price:'25'
+						}
+					],
+					sap:[
+						{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'丽人1',
+							pos:'地址1',
+							price:'25'
+						},{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'丽人2',
+							pos:'地址2',
+							price:'25'
+						},{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'丽人3',
+							pos:'地址3',
+							price:'25'
+						}
+					],
+					movie:[
+						{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'电影演出1',
+							pos:'地址1',
+							price:'25'
+						},{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'电影演出2',
+							pos:'地址2',
+							price:'25'
+						},{
+							url:'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+							title:'电影演出3',
+							pos:'地址3',
+							price:'25'
+						}
+					]				 
+			 
+			 }			 	 
+	  }
+	},
+	mounted () {
+		
+	},
+	methods:{
+	  enter(e){
+		let dom = e.target
+		let tag = dom.tagName.toLowerCase()
+		let self = this
+		if(tag === 'dd'){
+		  this.kind = dom.getAttribute('kind');  
+		}
+	  }	
+	},
+	computed:{
+      cur(){
+		  // console.log(this.list[this.kind])
+			return this.list[this.kind]
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+    @import "@/assets/css/index/artistic.scss";
+</style>
---
+++ vue-mtapp-master/components/index/life.vue
@@ -0,0 +1,67 @@
+<template>
+   <div class="m-life">
+     <el-row>
+			 <el-col :span="14">
+				  <Carousel/>
+			 </el-col>
+			  <el-col :span="4">
+         <div class="m-life-pic"></div>
+       </el-col>
+			 <el-col :span="6">
+				 <div class="m-life-login">
+					 <div class="m-life-login-tx"><img 
+            src="//s0.meituan.net/bs/fe-web-meituan/2d05c2b/img/avatar.jpg" 
+            alt=""></div>
+						<p class="m-life-login-name">Hi！你好</p>
+						<div>
+							<nuxt-link to="/register">
+								<el-button size="medium">注册</el-button>
+							</nuxt-link>							
+						</div>
+            <div>
+						<nuxt-link to="">
+							<el-button size="medium">立即登录</el-button>
+						</nuxt-link>	
+            </div>					
+				 </div>
+			 </el-col>
+		 </el-row>
+		 <el-row>
+			 <el-col :span="7">
+          <div class="m-life-hotel"></div> 
+			 </el-col>
+			 <el-col :span="7">
+				  <div class="m-life-music"></div> 
+			 </el-col>
+			 <el-col :span="4">
+				  <div class="m-life-coop"></div>
+			 </el-col>
+			 <el-col :span="6">
+			   <div class="m-life-downapp">
+				   <img src="//s1.meituan.net/bs/fe-web-meituan/60ac9a0/img/download-qr.png" 
+            alt="下载APP">
+						<p>美团APP手机版</p>
+						<h4><span class="red">1元起</span>吃喝玩乐</h4>
+				 </div>	 
+			 </el-col>			 
+		 </el-row>
+	 </div>
+</template>
+<script>
+import Carousel from './carousel'
+export default {
+	name:'',
+	components:{
+		Carousel
+	},
+	data(){
+	 return {
+
+	 }
+	}
+}
+</script>
+
+<style lang="scss">
+    @import "@/assets/css/index/life.scss";
+</style>
