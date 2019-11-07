79868030582d8b105c864e891cae101d281c7ec5
hicai <1021794763@qq.com>
2019-11-07 23:20:18 +0800

    xxx

---
+++ components/product/isSelect.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="m-product-select">
+    <dl class="tab">
+       <dt></dt>
+       <dd></dd> 
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+};
+</script>
+<style lang="scss" scoped>
+</style>
